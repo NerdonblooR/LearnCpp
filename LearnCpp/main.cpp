4b861e64e60838b71a8200872fb0cac07e601332
NerdonblooR <njbbtan@gmail.com>
2014-11-13 17:14:25 -0500

    Initial Commit

---
+++ LearnCpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnCpp
+//
+//  Created by Hao Tan on 2014-11-13.
+//  Copyright (c) 2014 Hao Tan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
